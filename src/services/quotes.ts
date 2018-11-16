24902701fb05ea497c2d2d1de8e6c88b6ffcb9ad
Unknown <rigrod1999@gmail.com>
2018-11-16 20:27:52 +0530

    The basic template of the QuoteApp Uploaded
    Angular , Ionic Contributors are welcomed

---
+++ src/services/quotes.ts
@@ -0,0 +1,27 @@
+import { Quote } from "../data/quote.interface";
+
+export class QuotesService {
+  private favoriteQuotes: Quote[] = [];
+
+  addQuoteToFavorites(quote: Quote) {
+    this.favoriteQuotes.push(quote);
+  }
+
+  removeQuoteFromFavorites(quote: Quote) {
+    const position = this.favoriteQuotes.findIndex((quoteEl: Quote) => {
+      return quoteEl.id == quote.id; // findIndex returns the index only if the condition given
+                                    //inside is true
+    });
+    this.favoriteQuotes.splice(position, 1);
+  }
+
+  getFavoriteQuotes() {
+    return this.favoriteQuotes.slice(); // slice() returns a copy of the array.
+   }
+
+  isQuoteFavorite(quote: Quote) {
+    return this.favoriteQuotes.find((quoteEl: Quote) => {
+      return quoteEl.id == quote.id;
+    });
+  }
+}
